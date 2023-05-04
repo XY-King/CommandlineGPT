3f2cf2395039e3a38a281eb24530d5be6216b37d
ausummer <ausummer.sjtu.edu.cn>
2023-05-04 22:33:27 +0800

    help message added

--- chatManager.py
+++ chatManager.py
@@ -16,6 +16,8 @@ def userInput(myChat):
             return values.BYE
         case "\t":
             return chatFunction.redirect(myChat)
+        case "help":
+            printHelpMessage()
         case _:
             myChat.addUserMessage(user_input)
             myChat.history.append({"role": "user", "content": user_input})
@@ -30,3 +32,10 @@ def GPTResponse(myChat):
         ) 
     myChat.addGPTMessage(response.choices[0].message.content)
     myChat.history.append({"role": "assistant", "content": response.choices[0].message.content})
+
+def printHelpMessage():
+    # print help message in purple
+    print("\033[95m" + "Help:" + "\033[0m")
+    print("\033[95m" + "1. Enter \"exit\" or \"quit\" or \"bye\" to exit the program." + "\033[0m")
+    print("\033[95m" + "2. Enter \"\t\" to redirect one response or user input in the conversation." + "\033[0m")
+    input()
